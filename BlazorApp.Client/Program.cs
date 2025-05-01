665ec6ed9c1a23cd6e3fd42ad50f584c818dd4df
JKal <jkaljkaljkal@yahoo.com>
2025-05-01 13:38:49 +0300

    Move Customers Page to BlazorApp.Client

--- BlazorApp.Client/Program.cs
+++ BlazorApp.Client/Program.cs
@@ -2,4 +2,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddHttpClient(
+    "WebApi",
+    client => client.BaseAddress = 
+        new Uri(builder.Configuration["ApiBaseUrl"]!));
+
+builder.Services.AddScoped(
+    sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WebApi"));
+
 await builder.Build().RunAsync();
